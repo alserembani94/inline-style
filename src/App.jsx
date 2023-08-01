d06b1d4fe212cfe1c4136b868b912504905bc1dc
Atif Aiman <alserembani94@gmail.com>
2023-08-01 22:14:31 +0800

    add inline styling

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HelloReact from "./components/HelloReact";
+
+function App() {
+  return (
+    <>
+      <HelloReact />
+      <HelloReact />
+      <HelloReact />
+      <HelloReact />
+      <HelloReact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,17 @@
+const HelloReact = () => {
+  return (
+    <p
+      style={{
+        backgroundColor: "aliceblue",
+        color: "grey",
+        fontSize: 36,
+        padding: "24px",
+        borderRadius: "1rem",
+      }}
+    >
+      Hello React
+    </p>
+  );
+};
+
+export default HelloReact;
